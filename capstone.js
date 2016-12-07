c2bd44eafd4bae7a7101114bf2c2bf8f4b225c46
Ryan Courtney <ryan.courtney1@gmail.com>
2016-12-07 11:32:53 -0500

    added json file for answers to questions

--- capstone.js
+++ capstone.js
@@ -19,77 +19,9 @@
 // JS throws no errors
 // User can play several rounds of new words
 // Hints are given to direct the user what the word is related to
-// HTML/CSS is properly formatted
-Capstone = {};
-Capstone.wordArray = [];
-Capstone.wordUsedArray =[];
+// HTML/CSS is properly formatted 
 
-Capstone.Lives = 9;
-Capstone.NumInWordBank = Words.length;
-Capstone.word = "test";
-Capstone.wordUsed="";
-
-
-var Capstone.pullWord = function(){
-	Capstone.word = Words.List[(Math.floor(Math.random()*Capstone.NumInWordBank))];
-}
-
-var Capstone.setUnderLine = function(){
-	Capstone.PullWord();
-	for(i=0; i<Capstone.Word.length; i++){
-		Capstone.wordArray[i] = Capstone.word.charAt(i);
-		Capstone.wordUsedArray[i] = "_";
-	}
-	Capstone.wordUsed = Capstone.wordUsedArray.join("");
-	document.getElementById("word").innerhtml = Capstone.wordUsed;
-
-}
-
-Capstone.setUnderLine = function(){
-	Capstone.pullWord();
-	for(i=0; i<Capston.word.length; i++){
-		Capstone.wordArray[i]=Capstone.word.charAt(i);
-		Capstone.wordArray[i] = "_";
-	}
-	Capstone.wordUsed = Capstone.wordUsedArray.join("");
-	document.getElementById("word").innerhtml = Capstone.wordUsed;
-
-}
-
-Capstone.updateLetter = function(letter){
-	Capstone.changes = 0;
-	for(i=0; i<Capstone.word.length; i++){
-		Capstone.wordArray[i] = Capstone.word.charAt(i);
-		if(Capstone.word.charAt(i) == letter){
-			Capstone.wordUsedArray[i] == letter;
-			Capstone.Changes += 1;
-		}
-	}
-}
-if(Capstone.Changes<1){
-	Capstone.Lives -=1;
-	document.getElementById("lives").innerhtml = Capstone.Lives;
-}
-
-Capstone.wordUsed = Capstone.wordUsedArray.join("");
-document.getElementById("word").innerhtml = Capstone.wordUsed;
-
-Capstone.word1 = Capstone.wordArray.join("");
-Capstone.word2 = Capstone.wordArray.join("");
-
-if(Capstone.word1 == Capstone.word2){
-	alert("You Win");
-	window.location.reload();
-}
-
-if(Capstone.Lives < 1){
-	document.getElementById("word").innerhtml == Capstone.word;
-	alert("You Lose")
-}
-
-Capstone.pullWord();
-Capstone.setUnderLine();
-
+var wordList["computer","newspaper", "age", "pigskin","television","cornucopia","rudolph","javascript","baseball"];
 
 
 
